6a62d58011fdb228b2ada38db3649aec095d8b9b
Arden Rasmussen <ardenisthebest@gmail.com>
2019-10-13 15:13:57 -0700

    Added notes on first lecture

--- build.r
+++ build.r
@@ -3,16 +3,16 @@
 args = commandArgs(trailingOnly=TRUE)
 
 if(length(args)==0) {
-  bookdown::render_book("01-introduction.Rmd", "bookdown::gitbook")
-  bookdown::render_book("01-introduction.Rmd", "bookdown::pdf_book")
-  bookdown::render_book("01-introduction.Rmd", "bookdown::epub_book")
+  bookdown::render_book("index.Rmd", "bookdown::gitbook")
+  bookdown::render_book("index.Rmd", "bookdown::pdf_book")
+  bookdown::render_book("index.Rmd", "bookdown::epub_book")
 } else if(length(args) == 1 && args[1] == "serve") {
   bookdown::serve_book(dir = ".", output_dir = "docs", preview = TRUE, in_session = TRUE, quiet = FALSE)
 } else if(length(args) == 1 && args[1] == "clean") {
   bookdown::clean_book(TRUE)
 } else {
-  bookdown::render_book("01-introduction.Rmd", "bookdown::gitbook")
-  bookdown::render_book("01-introduction.Rmd", "bookdown::pdf_book")
-  bookdown::render_book("01-introduction.Rmd", "bookdown::epub_book")
+  bookdown::render_book("index.Rmd", "bookdown::gitbook")
+  bookdown::render_book("index.Rmd", "bookdown::pdf_book")
+  bookdown::render_book("index.Rmd", "bookdown::epub_book")
 }
 
